80b96a67c30599021e59bd65183e3db681e21084
Richard Tortora <rtortora@craw.cc>
2013-06-24 23:23:26 -0400

    more stuff

---
+++ bubbles/none.rb
@@ -0,0 +1,4 @@
+bubble do |w, h|
+  pad_text 0, 0
+  render []
+end
---
+++ speakers/tinyturtle.rb
@@ -0,0 +1,7 @@
+tags %W(animal aquatic short small)
+speak <<'EOF'
+$BUBBLE$
+                                                  _  .----.
+                                                 (_\/      \_,
+                                                  'uu----uu~'
+EOF
